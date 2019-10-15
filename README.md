052e980aa3824ec048cb5c258d684ea652a3fb55
Aleksander Stós <aleksander.stos@gmail.com>
2019-10-15 12:41:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# eslint-config-swingdev-frontend-react
---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      project: './tsconfig.json',
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    plugins: ['@typescript-eslint', 'babel', 'react-hooks'],
+    extends: [
+      'standard',
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
+      'plugin:react/recommended',
+      'plugin:jsx-a11y/recommended',
+    ],
+    globals: {
+      localStorage: true,
+      fetch: true,
+      File: true,
+      FormData: true
+    },
+    rules: {
+      semi: [2, 'always'],
+      'react/prop-types': 0,
+      '@typescript-eslint/interface-name-prefix': 1,
+      '@typescript-eslint/ban-ts-ignore': 0,
+      'no-useless-constructor': 0,
+      'space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'never',
+          named: 'never',
+          asyncArrow: 'always'
+        }
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'react/jsx-wrap-multilines': [2, {
+        "declaration": "parens",
+        "assignment": "parens",
+        "return": "parens",
+        "arrow": "parens",
+        "condition": "parens",
+        "logical": "parens",
+        "prop": "parens"
+      }]
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  };
+  
